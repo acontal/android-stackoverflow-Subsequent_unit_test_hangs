41a4bffb1bd63038f4976947455f165048b70661
Lacton <lacton@users.sourceforge.net>
2014-01-16 21:15:24 +0100

    Init project.

---
+++ src/uk/co/example/activity/TargetActivity.java
@@ -0,0 +1,15 @@
+package uk.co.example.activity;
+
+import uk.co.example.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TargetActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_target);
+	}
+
+}
